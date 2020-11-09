a2e17d617b18ea08beefefedd4c1713ce014c2b1
Seagullie <59409233+Seagullie@users.noreply.github.com>
2020-11-09 21:36:47 +0200

    create react-practice folder

---
+++ react-practice/precache-manifest.cd029c3097ff7977e89505f9631613ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5731058b52e46598753369fc9aa7f417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217c64a7eb3eb070661b",
+    "url": "/static/css/main.933b1f28.chunk.css"
+  },
+  {
+    "revision": "7a96826a1d69b39594da",
+    "url": "/static/js/2.fc3ec09e.chunk.js"
+  },
+  {
+    "revision": "274f2f8729bafadc934e806e662fd688",
+    "url": "/static/js/2.fc3ec09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217c64a7eb3eb070661b",
+    "url": "/static/js/main.d5a30ed3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "63f7009e87bd9147769c50a4b5112e32",
+    "url": "/static/media/circle.63f7009e.svg"
+  },
+  {
+    "revision": "da839189569573ebc105c94123e6d1a4",
+    "url": "/static/media/close.da839189.svg"
+  },
+  {
+    "revision": "fd9df25fbcfe45e72f03503db86c5c46",
+    "url": "/static/media/seagull.fd9df25f.png"
+  }
+]);
